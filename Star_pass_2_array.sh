2d7cee326792e2db56d6ccca3c17ca2b8ff8a8d6
joannarifkin <joannarifkin@gmail.com>
2022-08-26 11:20:09 -0400

    Created second mapping job, corrected memory allocations in pass 1

--- Star_pass_2_array.sh
+++ Star_pass_2_array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=STAR_pass_1_Rifkin
+#SBATCH --job-name=STAR_pass_2_Rifkin
 #SBATCH --mail-user=#SBATCH --mail-user=jlrifkin@umich.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --cpus-per-task=6
@@ -15,7 +15,7 @@
 
 #module load Bioinformatics
 #module load Bioinformatics gcc/10.3.0-k2osx5y
-#module load trimgalore
+#module load star/2.7.6a-aekjdpr
 
 file=$(ls SRR134494*.fastq.gz | sed -n ${SLURM_ARRAY_TASK_ID}p)
 #awk '{sub(/.*/, ""); print}' $file
@@ -26,11 +26,15 @@ echo $file
 echo $samplename
 
 mkdir -p ./STAR/
-mkdir -p ./STAR/Pass1/
+mkdir -p ./STAR/Pass2/
 
 STAR --runThreadN 6 \
 --genomeDir /home/jlrifkin/Analyses/Transcriptome/STAR/Ipomoea_reference \
 --readFilesIn $file \
 --readFilesCommand zcat \
---outFileNamePrefix ./STAR/Pass1/$samplename \
---outSAMtype BAM Unsorted
+--outFileNamePrefix ./STAR/Pass2/$samplename \
+--outSAMtype BAM SortedByCoordinate \
+--sjdbFileChrStartEnd /STAR/SJ.all.tab
+
+
+
