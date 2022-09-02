d60c5e5585e808e3235bb998f6287e5399c270a9
joannarifkin <joannarifkin@gmail.com>
2022-09-02 14:32:33 -0400

    Started setting up SLURM script to index using RSubread

---
+++ RSubread_index.R
@@ -0,0 +1,17 @@
+getwd()
+setwd("/home/jlrifkin/Analyses/Transcriptome/RSubread")
+
+
+#These two commands are necessary the first time you use RSubread
+
+#if (!require("BiocManager", quietly = TRUE))
+  # install.packages("BiocManager")
+
+#BiocManager::install("Rsubread") #Note that this may 
+library(Rsubread)
+ref <- system.file("extdata","reference.fa",package="Rsubread") #Demo genome and index, included with the package
+buildindex(basename="reference_index",reference=ref) #Note that this will build in the current working directory. Make sure to setwd() to the place you want the index to be!
+
+ipomoea_ref<-"/nfs/turbo/rsbaucom/lab/IP_genome_COGE/Ipomoea_purpurea_headings_fixed.faa"
+
+buildindex(basename="Ipomoea_index",reference=ipomoea_ref)
