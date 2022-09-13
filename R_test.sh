358c0fb09f299321f64634e4d30929fa415aeb25
joannarifkin <joannarifkin@gmail.com>
2022-09-13 15:40:03 -0400

    How to actually do things in R in Slurm - wrapper script

---
+++ R_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --account=rsbaucom0
+#SBATCH --job-name=SLURM_index_test
+#SBATCH --mail-user=jlrifkin@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --cpus-per-task=2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=1GB
+#SBATCH --time=20:00
+#SBATCH --partition=standard
+#SBATCH --output=RSubread_index-%A-%a.log
+
+
+
+module load R
+R CMD BATCH R_test.R
+#
+#myvar <- randnorm(mean=0,sd=1,n=10)
+#write(myvar, "file.txt")
--- SLURM_RSubread_index.sh
+++ SLURM_RSubread_index.sh
@@ -1,16 +1,27 @@
-###NOT FINISHED
+#!/bin/bash
 
-getwd()
-setwd("/home/jlrifkin/Analyses/Transcriptome/RSubread")
+#SBATCH --account=rsbaucom0
+#SBATCH --job-name=SLURM_index_test
+#SBATCH --mail-user=jlrifkin@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --cpus-per-task=2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=1GB
+#SBATCH --time=20:00
+#SBATCH --partition=standard
+#SBATCH --output=RSubread_index-%A-%a.log
 
 
-#These two commands are necessary the first time you use RSubread
 
-#if (!require("BiocManager", quietly = TRUE))
-  # install.packages("BiocManager")
+module load R
+R
 
-#BiocManager::install("Rsubread") #Note that this may 
-library(Rsubread)
+Rserve(args="--no-save")
+
+library("Rsubread",lib="/nfs/turbo/rsbaucom/lab/SOFTWARE/R/4.2/library")
+setwd("/home/jlrifkin/Analyses/Transcriptome/RSubread/test") #directory where you want to run it
+
 ref <- system.file("extdata","reference.fa",package="Rsubread") #Demo genome and index, included with the package
 buildindex(basename="reference_index",reference=ref) #Note that this will build in the current working directory. Make sure to setwd() to the place you want the index to be!
 
